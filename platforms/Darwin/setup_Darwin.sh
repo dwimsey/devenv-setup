c98b19aa1e688e3dfe4e8aed823664bd64a30062
David Wimsey <david@wimsey.us>
2016-08-31 09:05:43 -0400

    Adding maven and IntelliJ bits

--- platforms/Darwin/setup_Darwin.sh
+++ platforms/Darwin/setup_Darwin.sh
@@ -1,6 +1,10 @@
 #/usr/bin/env sh
 VAGRANTVERSION=1.8.5
+INTELLIJVERSION=2016.2.2
+INTELLIJEDITION=U
 VBPKGFILE=VirtualBox-5.1.2-108956-OSX.dmg
+JDKPKGFILE=jdk-8u101-macosx-x64.dmg
+IJPKGFILE=ideaIU-2016.2.2.dmg
 SSHKEYFILE="${HOME}/.ssh/id_rsa"
 
 echo OSX development environment boot script
@@ -41,8 +45,29 @@ sudo hdiutil detach /Volumes/VirtualBox
 # Windows
 # http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-windows-x64.exe
 
-JDKPKGFILE=jdk-8u101-macosx-x64.dmg
 curl -o /tmp/${JDKPKGFILE} -L -b "oraclelicense=a" http://download.oracle.com/otn-pub/java/jdk/8u101-b13/${JDKPKGFILE}
 sudo hdiutil attach /tmp/${JDKPKGFILE}
 sudo installer -pkg "/Volumes/JDK 8 Update 101/JDK 8 Update 101.pkg" -target /
 sudo hdiutil detach "/Volumes/JDK 8 Update 101"
+
+# Install homebrew, lets us do maven easily
+
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#sudo brew install maven
+
+MVNPKGFILE=apache-maven-3.3.9-bin.tar.gz
+curl -L -o /tmp/${MVNPKGFILE} http://apache.claz.org/maven/maven-3/3.3.9/binaries/${MVNPKGFILE}
+sudo mkdir -p /usr/local
+tar zxf /tmp/${MVNPKGFILE}
+mv apache-maven-3.3.9-bin /usr/local/maven
+sudo chown -R root:staff /usr/local/maven
+ln -s /usr/local/maven/bin/mvn /usr/local/bin
+
+IJPKGFILE=ideaI${INTELLIJEDITION}-${INTELLIJVERSION}.dmg
+curl -o /tmp/${IJPKGFILE} https://download-cf.jetbrains.com/idea/${IJPKGFILE}
+sudo hdiutil attach /tmp/${IJPKGFILE}
+sudo mv "/Volumes/IntelliJ IDEA/IntelliJ IDEA.app" /Applications
+sudo hdiutil detach "/Volumes/IntelliJ IDEA"
+
+
+
